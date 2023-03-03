5fec68d8f14dd4bcdef8f73a784f6b43c686a64e
Damian Cendan <59464234+DamianCendan@users.noreply.github.com>
2023-03-03 00:59:28 -0800

    148. Exercise: Applying Colors

--- build/blocks/search-form/index.asset.php
+++ build/blocks/search-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => '5e95b8c706687c3ce453');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b318332a85928a026aa1');
